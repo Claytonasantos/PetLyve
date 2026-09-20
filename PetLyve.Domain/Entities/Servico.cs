6d9f756ed9c964b97108720714b81dd9487f12de
Claytonasantos <claytonalves2579@gmail.com>
2026-09-19 23:23:40 -0300

    feat: adicionar validacao de preco no servico

--- PetLyve.Domain/Entities/Servico.cs
+++ PetLyve.Domain/Entities/Servico.cs
@@ -12,4 +12,14 @@ public class Servico : BaseEntity
     public decimal Preco { get; set; }
 
     public List<Agendamento> Agendamentos { get; set; } = new List<Agendamento>();
+
+    public void AlterarPreco(decimal novoPreco)
+    {
+        if (novoPreco <= 0)
+        {
+            throw new ArgumentException("O preço deve ser maior que zero.");
+        }
+
+        Preco = novoPreco;
+    }
 }
